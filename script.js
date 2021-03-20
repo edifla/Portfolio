34a6261891c4239f6804d5895626d6d17e2a01e9
Rafael Albuquerque <54068660+edifla@users.noreply.github.com>
2021-03-20 13:24:43 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const Modal = {
-    open(imagem) {
-        document.querySelector('.modal')
-        .classList
-        .add('active')
-        let img = document.querySelector('.imagem-modal')
-        img.src = imagem.src.replace('small','big')
-    
-    },
-    close() {
-        document.querySelector('.modal')
-        .classList
-        .remove('active')
-        let img = document.querySelector('.imagem-modal')
-        img.src = "#"
-    }
+	open(imagem) {
+		document.querySelector('.modal')
+			.classList
+			.add('active')
+		let img = document.querySelector('.imagem-modal')
+		img.src = imagem.src.replace('small', 'big')
+
+	},
+	close() {
+		document.querySelector('.modal')
+			.classList
+			.remove('active')
+		let img = document.querySelector('.imagem-modal')
+		img.src = "#"
+	}
 }
